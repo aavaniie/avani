dacec41ee32a259fe933f83624bc988c9e43226f
avani <avanimkp@gmail.com>
2026-07-18 18:06:39 +0530

    content

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -17,7 +17,7 @@ function Projects() {
             </div>
             <div className="proj-thumb">Hapx Digital</div>
             <div className="proj-body">
-              <div className="proj-title">Hapx Digital [Client Project - Hapx Digital]</div>
+              <div className="proj-title">Hapx Digital <br />Client Project - Hapx Digital</div>
               <div className="proj-desc">
                 Created a responsive, SEO‑optimized website for a branding firm, ensuring fast load times, cross‑browser
 compatibility, and a user‑friendly design using modern HTML, CSS, and JavaScript practices
@@ -36,7 +36,7 @@ compatibility, and a user‑friendly design using modern HTML, CSS, and JavaScri
             </div>
             <div className="proj-thumb">FranGulf</div>
             <div className="proj-body">
-              <div className="proj-title">FranGulf [Client Project - Hapx Digital]</div>
+              <div className="proj-title">FranGulf <br />Client Project - Hapx Digital</div>
               <div className="proj-desc">
                 Designed and developed the official website for Frangulf, a UAE‑based law firm, focusing on modern, responsive design and clear usability across devices.
               </div>
@@ -54,7 +54,7 @@ compatibility, and a user‑friendly design using modern HTML, CSS, and JavaScri
             </div>
             <div className="proj-thumb">Samuhik Pahal</div>
             <div className="proj-body">
-              <div className="proj-title">Portfolio Website [Client Project – Midnay Tech Private Limited]</div>
+              <div className="proj-title">Portfolio Website <br />Client Project – Midnay Tech Private Limited</div>
               <div className="proj-desc">
                 Developed a fully responsive, high‑performance website for Samuhik Pahal, ensuring fast page speed, reliable behavior, and a consistent experience across devices and browsers.
 
